7eaaf9eb674e6e29eddc29c7f5d7f1e04b6cfe97
Mike <k2punk@gmail.com>
2022-07-26 12:46:44 +0300

    finish second step

--- __tests__/downloadPage.test.js
+++ __tests__/downloadPage.test.js
@@ -1,7 +1,9 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, beforeEach } from '@jest/globals';
 import * as path from 'path';
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
+import nock from 'nock';
+import os from 'os';
 import downloadPage from '../src/downloadPage.js';
 
 const { promises: fsp } = fs;
@@ -10,15 +12,40 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('save file', () => {
-  downloadPage('https://ru.hexlet.io/courses', '/usr');
-  const fileExpected = fsp.readFile(getFixturePath('expected.html'), 'utf-8');
-  const file = fsp.readFile(path.resolve('/usr', 'ru-hexlet-io-courses.html'), 'utf-8');
-  expect(file).toEqual(fileExpected);
+let dir;
+
+beforeEach(async () => {
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, await fsp.readFile(getFixturePath('source.html'), 'utf-8'));
+  nock('https://ru.hexlet.io')
+    .get('/courses//assets/professions/nodejs.png')
+    .reply(200, await fsp.readFile(getFixturePath('nodejs.png')));
+  dir = await fsp.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
+
+test('save file', async () => {
+  await downloadPage('https://ru.hexlet.io/courses', dir);
+  const file = await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses.html'), 'utf-8');
+  expect(file).not.toBeNull();
 });
 
 test('return right object', async () => {
   const file = await fsp.readFile(getFixturePath('expected.json'), 'utf-8');
   const object = JSON.parse(file);
-  expect(downloadPage('https://ru.hexlet.io/courses', '/usr')).toEqual(object);
+  expect(await downloadPage('https://ru.hexlet.io/courses', '/usr')).toEqual(object);
+});
+
+test('modify page', async () => {
+  await downloadPage('https://ru.hexlet.io/courses', dir);
+  const fileExpected = await fsp.readFile(getFixturePath('downloaded.html'), 'utf-8');
+  const file = await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses.html'), 'utf-8');
+  expect(file).toEqual(fileExpected);
+});
+
+test('save image', async () => {
+  await downloadPage('https://ru.hexlet.io/courses', dir);
+  const image = await fsp.readFile(getFixturePath('nodejs.png'));
+  const copydImage = await fsp.readFile(path.resolve(dir, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-assets-professions-nodejs.png'));
+  expect(image).toEqual(copydImage);
 });
--- src/downloadPage.js
+++ src/downloadPage.js
@@ -1,14 +1,47 @@
 import * as path from 'path';
 import fs from 'fs';
 import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+const { promises: fsp } = fs;
+
+const getAssets = (page, url, fullDirPath, dirPath, prefix) => {
+  const $ = cheerio.load(page);
+  const imageTag = $('img');
+  const src = Array.from(imageTag).map((element) => $(element).attr('src'));
+  src.forEach((el) => {
+    if (path.extname(el) === '.png' || path.extname(el) === '.jpg') {
+      axios({
+        method: 'get',
+        url: `${url}/${el}`,
+        responseType: 'stream',
+      })
+        .then(({ data }) => {
+          const normalizedStr = `${prefix}${el.replace(/\//g, '-')}`;
+          return fsp.writeFile(path.join(fullDirPath, normalizedStr), data);
+        });
+    }
+  });
+  $('img').each(function modify(i, elem) {
+    $(this).attr('src', `${dirPath}/${prefix}${$(elem).attr('src').replace(/\//g, '-')}`);
+  });
+  return $.html();
+};
 
 export default (url, dir = process.cwd()) => {
   const myURL = new URL(url);
-  const fileName = `${myURL.hostname.replace(/\./g, '-')}${myURL.pathname.replace('/', '-')}.html`;
+  const fileName = myURL.pathname !== '/' ? `${myURL.hostname.replace(/\./g, '-')}${myURL.pathname.replace('/', '-')}.html` : `${myURL.hostname.replace(/\./g, '-')}.html`;
+  const dirName = myURL.pathname !== '/' ? `${myURL.hostname.replace(/\./g, '-')}${myURL.pathname.replace('/', '-')}_files` : `${myURL.hostname.replace(/\./g, '-')}_files`;
+  const assetsName = myURL.pathname !== '/' ? `${myURL.hostname.replace(/\./g, '-')}` : `${myURL.hostname.replace(/\./g, '-')}-`;
   const filePath = path.resolve(process.cwd(), dir, fileName);
-  axios.get(url)
-    .then(({ data }) => fs.writeFileSync(filePath, data));
-  console.log(`Page was successfully downloaded into ${filePath}`);
-  const obj = { filepath: filePath };
-  return obj;
+  const dirPath = path.resolve(process.cwd(), dir, dirName);
+  return fsp.mkdir(dirPath)
+    .then(() => axios.get(url))
+    .then(({ data }) => {
+      const modifiedPage = getAssets(data, url, dirPath, dirName, assetsName);
+      fsp.writeFile(filePath, modifiedPage);
+      console.log(`Page was successfully downloaded into ${filePath}`);
+      const obj = { filepath: filePath };
+      return obj;
+    });
 };
